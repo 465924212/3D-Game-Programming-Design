eec894e5463ed74956458c2a9b9f4748674ef0ae
465924212 <31825305+465924212@users.noreply.github.com>
2018-05-29 21:42:03 +0800

    8.0
    叭叭叭

---
+++ 八/BallControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    ParticleSystem ball;
+    //三个属性对应RGB属性
+    public float temperature;
+    public float height;
+    public float humidity;
+
+	// Use this for initialization
+	void Start () {
+        ball = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var main = ball.main;
+        main.startColor = new Color(temperature/255, height/255, humidity/255);
+	}
+}
